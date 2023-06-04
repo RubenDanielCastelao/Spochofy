3b00e31ce9b60c070978fa5f3073b9beedf0f125
Borja G. Barrera <bgarciabarrera@danielcastelao.org>
2023-06-04 21:21:19 +0200

    Llevada a cabo la documentación de todos los métodos de la clase CancionesLR.java.

--- src/main/java/BD/CancionesLR.java
+++ src/main/java/BD/CancionesLR.java
@@ -4,23 +4,44 @@ public class CancionesLR {
 
     int lid, cid;
 
+    /**
+     *
+     * @param lid
+     * @param cid
+     */
     public CancionesLR(int lid, int cid) {
         this.lid = lid;
         this.cid = cid;
     }
 
+    /**
+     *
+     * @return lid
+     */
     public int getLid() {
         return lid;
     }
 
+    /**
+     *
+     * @param lid
+     */
     public void setLid(int lid) {
         this.lid = lid;
     }
 
+    /**
+     *
+     * @return cid
+     */
     public int getCid() {
         return cid;
     }
 
+    /**
+     *
+     * @param cid
+     */
     public void setCid(int cid) {
         this.cid = cid;
     }
