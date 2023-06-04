0d281b364a37c5db0f99c0dd998c3c6a975f628f
Borja G. Barrera <bgarciabarrera@danielcastelao.org>
2023-06-04 18:33:17 +0200

    Refs #3 Creada clase CancionesLR.java para poder instanciar objetos de este tipo

---
+++ src/main/java/BD/CancionesLR.java
@@ -0,0 +1,27 @@
+package BD;
+
+public class CancionesLR {
+
+    int lid, cid;
+
+    public CancionesLR(int lid, int cid) {
+        this.lid = lid;
+        this.cid = cid;
+    }
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+}
