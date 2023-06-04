81afbc6c0ec15cba5fe4a22875827fa5cc031674
Borja G. Barrera <bgarciabarrera@danielcastelao.org>
2023-06-04 21:21:10 +0200

    Llevada a cabo la documentación de todos los métodos de la clase Canciones.java.

--- src/main/java/BD/Canciones.java
+++ src/main/java/BD/Canciones.java
@@ -8,6 +8,14 @@ public class Canciones {
     int cid, yid, aid, atid;
     String ctitulo;
 
+    /**
+     *
+     * @param cid
+     * @param yid
+     * @param aid
+     * @param atid
+     * @param ctitulo
+     */
     public Canciones(int cid, int yid, int aid, int atid, String ctitulo) {
         this.cid = cid;
         this.yid = yid;
@@ -16,42 +24,82 @@ public class Canciones {
         this.ctitulo = ctitulo;
     }
 
+    /**
+     *
+     * @return cid
+     */
     public int getCid() {
         return cid;
     }
 
+    /**
+     *
+     * @param cid
+     */
     public void setCid(int cid) {
         this.cid = cid;
     }
 
+    /**
+     *
+     * @return yid
+     */
     public int getYid() {
         return yid;
     }
 
+    /**
+     *
+     * @param yid
+     */
     public void setYid(int yid) {
         this.yid = yid;
     }
 
+    /**
+     *
+     * @return aid
+     */
     public int getAid() {
         return aid;
     }
 
+    /**
+     *
+     * @param aid
+     */
     public void setAid(int aid) {
         this.aid = aid;
     }
 
+    /**
+     *
+     * @return atid
+     */
     public int getAtid() {
         return atid;
     }
 
+    /**
+     *
+     * @param atid
+     */
     public void setAtid(int atid) {
         this.atid = atid;
     }
 
+    /**
+     *
+     * @return ctitulo
+     */
     public String getCtitulo() {
         return ctitulo;
     }
 
+    /**
+     *
+     * @param ctitulo
+     */
     public void setCtitulo(String ctitulo) {
         this.ctitulo = ctitulo;
     }
