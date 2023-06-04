b6b5b2ef2d0fdd05096cb30f9384152f22046114
Borja G. Barrera <bgarciabarrera@danielcastelao.org>
2023-06-04 18:32:48 +0200

    Refs #3 Creada clase Anyos.java para poder instanciar objetos de este tipo

---
+++ src/main/java/BD/Anyos.java
@@ -0,0 +1,28 @@
+package BD;
+
+public class Anyos {
+
+    int yid;
+    String ynumero;
+
+    public Anyos(int yid, String ynumero) {
+        this.yid = yid;
+        this.ynumero = ynumero;
+    }
+
+    public int getYid() {
+        return yid;
+    }
+
+    public void setYid(int yid) {
+        this.yid = yid;
+    }
+
+    public String getYnumero() {
+        return ynumero;
+    }
+
+    public void setYnumero(String ynumero) {
+        this.ynumero = ynumero;
+    }
+}
