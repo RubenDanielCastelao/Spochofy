4f014ae28e1e826b171d60f9a73e954cf9dac633
Borja G. Barrera <bgarciabarrera@danielcastelao.org>
2023-06-04 16:52:44 +0200

    Refs #3 Modificación de métodos para adaptar a SQLite la conexión y desconexión a la base de datos

---
+++ src/main/java/Clases/Enlace.java
@@ -0,0 +1,64 @@
+package Clases;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase para la conexión a la base de datos.
+ */
+
+public class Enlace {
+
+
+    /**
+     * Atributos para la conexión a la base de datos.
+     */
+    Connection conexion;
+    private static Enlace conectar = null;
+    String url = "jdbc:sqlite:src/database/spochofy.db";
+
+
+
+    /**
+     * Conexión a la base de datos.
+     */
+    private Enlace() {
+
+        try {
+            conexion = DriverManager.getConnection(url);
+            if (conexion != null) {
+                JOptionPane.showMessageDialog(null,"Conexión a base de datos realizada con éxito.");
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Conexión a base de datos fallida: " + e);
+        }
+    }
+
+    /**
+     * Singleton para la conexión a la base de datos.
+     * @return
+     */
+    public static Enlace getInstance() {
+        if (conectar == null) {
+            conectar = new Enlace();
+        }
+        return conectar;
+    }
+
+    /**
+     * Método para cerrar la conexión a la base de datos.
+     */
+    public void desconexion() {
+        try {
+            conexion.close();
+            JOptionPane.showMessageDialog(null, "Desconexión a base de datos realizada con éxito.");
+        } catch (SQLException ex) {
+            Logger.getLogger(Enlace.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
